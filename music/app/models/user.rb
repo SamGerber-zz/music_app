94ca7cb971c0b30b2dc5af9fddac17aa359c6aa3
SamGerber <gerbyxvi@gmail.com>
2016-01-28 11:22:31 -0800

    Document each method

--- music/app/models/user.rb
+++ music/app/models/user.rb
@@ -16,29 +16,35 @@ class User < ActiveRecord::Base
 
   after_initialize :ensure_session_token
 
+  # Generate a random token
   def self.generate_session_token
     SecureRandom.urlsafe_base64
   end
 
+  # Fetches a user by email and password, returning nil when none are found
   def self.find_by_credentials(email:, password:)
     user = User.find_by(email: email)
     return user if user.exists? && user.is_password?(password)
   end
 
+  # Sets a new session_token and saves the user
   def reset_session_token!
     self.session_token = User.generate_session_token
     save!
   end
 
+  # If the user does not yet have a session_token, one is generated
   def ensure_session_token
     reset_session_token! unless session_token
   end
 
+  # Creates password digest
   def password=(object)
     @password = String(object)
     self.password_digest = String(BCrypt::Password.create(@password))
   end
 
+  # Checks if the input creates the correct digest.
   def is_password?(object)
     BCrypt::Password.new(password_digest).is_password?(String(object))
   end
