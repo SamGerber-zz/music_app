4d1668ab244645397abb5e5223b6a649b917fbd0
SamGerber <gerbyxvi@gmail.com>
2016-01-28 15:56:16 -0800

    Add albums resource, nest :new and :index in bands

--- music/config/routes.rb
+++ music/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :show, :create]
   resource :session, only: [:new, :create, :destroy]
-  resources :bands
+  resources :bands do
+    resources :albums, only: [:new, :index]
+  end
+  resources :albums, only: [:create, :edit, :show, :update, :destroy]
 end
