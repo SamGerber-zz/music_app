ca8033555d3e78b761ae44fb57207353d004afbf
SamGerber <gerbyxvi@gmail.com>
2016-01-28 14:52:23 -0800

    Annotate with schema

--- music/app/models/album.rb
+++ music/app/models/album.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: albums
+#
+#  id         :integer          not null, primary key
+#  title      :string           not null
+#  band_id    :integer          not null
+#  venue      :string(1)        not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Album < ActiveRecord::Base
   VENUES = {
     l: :live,
--- music/app/models/track.rb
+++ music/app/models/track.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: tracks
+#
+#  id         :integer          not null, primary key
+#  album_id   :integer          not null
+#  name       :string           not null
+#  bonus      :string(1)        not null
+#  lyrics     :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Track < ActiveRecord::Base
   BONUS = {
     b: :bonus,
