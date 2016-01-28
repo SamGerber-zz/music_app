410740b5416159bd3927ee47757d4b48c8c900b0
SamGerber <gerbyxvi@gmail.com>
2016-01-28 12:35:48 -0800

    Write log_in methods
     - #log_in!(user) logs a user in and logs out any other clients
     - #log_out! logs a user out on all clients
     - #current_user fetches a current user or returns nil on failure
     - #logged_in? and #logged_out? predicate methods

--- music/app/controllers/application_controller.rb
+++ music/app/controllers/application_controller.rb
@@ -2,4 +2,27 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def log_in!(user)
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def log_out!
+    session[:session_token] = nil
+    current_user.session_token = nil
+    current_user.save!
+    @current_user = nil
+  end
+
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def logged_in?
+    current_user
+  end
+
+  def logged_out?
+    !logged_in?
+  end
 end
