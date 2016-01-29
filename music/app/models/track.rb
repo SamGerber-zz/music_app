22f3820996cc4a0f5dca5f13f9c06c68d4fda9c9
SamGerber <gerbyxvi@gmail.com>
2016-01-28 18:29:41 -0800

    Stringify BONUS, rename artist relation as band
     - Change the keys and values in bonus to be strings
     - rename the relation artist to band

--- music/app/models/track.rb
+++ music/app/models/track.rb
@@ -13,16 +13,16 @@
 
 class Track < ActiveRecord::Base
   BONUS = {
-    b: :bonus,
-    r: :regular
+    "b" => "bonus",
+    "r" => "regular"
   }
   validates :album_id, :name, :bonus, presence: true
   validates :bonus, inclusion: { in: BONUS.keys }
 
   belongs_to :album
 
-  has_one :artist,
+  has_one :band,
     through: :album,
-    source: :artist
+    source: :band
 
 end
