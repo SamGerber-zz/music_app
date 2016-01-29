033182f7901d9bd3e6bb9e21918216116bd99c08
SamGerber <gerbyxvi@gmail.com>
2016-01-28 18:36:34 -0800

    Seed tracks in each album

--- music/db/seeds.rb
+++ music/db/seeds.rb
@@ -10,5 +10,16 @@ User.create!(email: "sam@example.com", password: "asdasd")
     options[:venue] = Album::VENUES.keys.sample
     album = Album.new(options)
     album.save
+    (1 + rand(20)).times do
+      options = {}
+      options[:album_id] = album.id
+      options[:name] = Faker::Hipster.words(1 + rand(4)).join(" ").capitalize
+      options[:bonus] = Track::BONUS.keys.sample
+      lyrics = []
+      5.times { lyrics << Faker::Hipster.paragraph(2, false, 4) }
+      options[:lyrics] = lyrics.join("\n\n")
+      track = Track.new(options)
+      track.save
+    end
   end
 end
