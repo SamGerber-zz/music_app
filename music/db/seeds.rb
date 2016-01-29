d838b9660a7dbc54df7aa87a7b022d108e550f41
SamGerber <gerbyxvi@gmail.com>
2016-01-28 16:42:55 -0800

    Seed db with albums

--- music/db/seeds.rb
+++ music/db/seeds.rb
@@ -1,5 +1,14 @@
 User.create!(email: "sam@example.com", password: "asdasd")
 
 10.times do
-  Band.create(name: Faker::Book.title)
+  band = Band.new(name: Faker::Book.title)
+  band.save
+  rand(10).times do
+    options = {}
+    options[:band_id] = band.id
+    options[:title] = Faker::Company.bs
+    options[:venue] = Album::VENUES.keys.sample
+    album = Album.new(options)
+    album.save
+  end
 end
